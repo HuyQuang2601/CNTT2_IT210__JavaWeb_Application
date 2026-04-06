4fa808d28c30b676d205c46a92c530af7eae488f
huy <huyxauzai2601@gmail.com>
2026-04-06 23:52:33 +0700

    update ex ss02

---
+++ Ss2/Ss2-Ex5/src/main/java/org/example/ss2Ex5/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.example.ss2Ex5.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "org.example.ss2Ex5.controller")
+public class WebConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resourceViewResolver = new InternalResourceViewResolver();
+        resourceViewResolver.setPrefix("/WEB-INF/views/");
+        resourceViewResolver.setSuffix(".jsp");
+        return resourceViewResolver;
+    }
+}
---
+++ Ss2/Ss2-Ex5/src/main/java/org/example/ss2Ex5/controller/LoginController.java
@@ -0,0 +1,26 @@
+package org.example.ss2Ex5.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(@RequestParam(name = "username", required = false) String username,
+                        @RequestParam(name = "password", required = false) String password,
+                        HttpSession session,
+                        Model model) {
+        if (username.equals("hr_manager") && password.equals("hr123") ||
+                username.equals("hr_staff") && password.equals("staff456")) {
+
+            session.setAttribute("username", username);
+            session.setAttribute("password", password);
+            return "redirect:/employees";
+        }
+        model.addAttribute("error", "Thông tin đăng nhập không chính xác");
+        return "login";
+    }
+}
